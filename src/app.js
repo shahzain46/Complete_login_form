f7e97eacffb0b068ea12486eabe2bf6b85a78641
shahzain46 <shahzain6016@gmail.com>
2023-08-08 17:17:44 -0700

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
 const app = express();
 const port = process.env.PORT || 3000;
 require('./db/conn')
 
 
 
+const static_path = path.join(__dirname , '../public');
+const template_path = path.join(__dirname , '../templates/views');
+const partials_path = path.join(__dirname , '../templates/partials');
+
+
+app.use(express.static(static_path));
+app.set('view engine', ' hbs');
+app.set('views', template_path);
+// app.set('view engine', 'html');
+// app.engine('html', require('hbs').__express);
+
+hbs.registerPartials(partials_path)
+
+
+
 app.get('/',(req,res)=>{
-    res.send('im shahzain')
+    res.render('index.hbs')
 })
 
 
@@ -15,5 +33,5 @@ app.get('/',(req,res)=>{
 
 
 app.listen(port , ()=>{
-    console.log(`server is listen to ${port}`);
+    console.log(`server is listen to port number ${port}`);
 })
