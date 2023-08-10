0e6266a7056dce096b23f9e6ddc5b8abcef9a739
shahzain46 <shahzain6016@gmail.com>
2023-08-10 06:25:02 -0700

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const Register = require('./models/registers')
+const  {json}  = require('express')
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,10 +11,15 @@ require('./db/conn')
 
 
 
+
+
 const static_path = path.join(__dirname , '../public');
 const template_path = path.join(__dirname , '../templates/views');
 const partials_path = path.join(__dirname , '../templates/partials');
 
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.use(express.static(static_path));
 app.set('view engine', ' hbs');
@@ -31,6 +39,45 @@ app.get('/',(req,res)=>{
 app.get('/register',(req,res)=>{
     res.render('register.hbs')
 })
+
+
+app.post('/register', async (req,res)=>{
+
+    const data={
+        phone:req.body.phone,
+        email:req.body.email,
+        password:req.body.password,
+       conformpassword:req.body.conformpassword
+    }
+
+    await Register.insertMany([data])
+
+    res.render('index.hbs')
+})
+
+    // try{
+    //     const password = req.body.password
+    //     const cPassword = req.body.conformpassword
+
+    //     if(password = cPassword){
+    //         const registerEmployee = new Register({
+    //             phone:req.body.phone,
+    //             email:req.body.email,
+    //             password:password,
+    //             conformpassword:cPassword
+    //         })
+    //     const  registered = await registerEmployee.save();
+    //     res.status(201).render('index.hbs')
+
+    //     }else{
+    //         res.send("password are not matching")
+    //     }
+
+    // }catch(error){
+    //     res.status(400).send(error)
+
+    // }
+// })
 
 
 
